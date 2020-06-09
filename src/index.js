4fec5e287296173b55a11dfc50e56559a502a840
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-06-10 01:30:30 +0200

    add fragment, pagination, orderBy with stars

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-import ApolloClient, { gql } from 'apollo-boost';
-import 'cross-fetch/polyfill';
 import 'dotenv/config';
+import 'cross-fetch/polyfill';
+import ApolloClient, { gql } from 'apollo-boost';
 
-const GET_REPOSITORIES_OF_ORGANIZATION = gql`
-  query($organization: String!) {
-    organization(login: $organization) {
-      name
-      url
-      repositories(first: 5) {
-        edges {
-          node {
-            name
-            url
-          }
-        }
-      }
-    }
-  }
-`;
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   request: (operation) => {
@@ -29,11 +13,75 @@ const client = new ApolloClient({
   },
 });
 
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  query($organization: String!, $cursor: String) {
+    organization(login: $organization) {
+      name
+      url
+      repositories(
+        first: 5
+        orderBy: { direction: DESC, field: STARGAZERS }
+        after: $cursor
+      ) {
+        edges {
+          node {
+            ...repository
+          }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
+    }
+  }
+  fragment repository on Repository {
+    name
+    url
+  }
+`;
+
 client
   .query({
     query: GET_REPOSITORIES_OF_ORGANIZATION,
     variables: {
       organization: 'the-road-to-learn-react',
+      cursor: undefined,
     },
   })
-  .then(console.log);
+  // resolve first page
+  .then((result) => {
+    const { pageInfo, edges } = result.data.organization.repositories;
+    const { endCursor, hasNextPage } = pageInfo;
+
+    console.log('first page', edges.length);
+    console.log('endCursor', endCursor);
+
+    return pageInfo;
+  })
+  // query second page
+  .then(({ endCursor, hasNextPage }) => {
+    if (!hasNextPage) {
+      throw Error('no next page');
+    }
+
+    return client.query({
+      query: GET_REPOSITORIES_OF_ORGANIZATION,
+      variables: {
+        organization: 'the-road-to-learn-react',
+        cursor: endCursor,
+      },
+    });
+  })
+  // resolve second page
+  .then((result) => {
+    const { pageInfo, edges } = result.data.organization.repositories;
+    const { endCursor, hasNextPage } = pageInfo;
+
+    console.log('second page', edges.length);
+    console.log('endCursor', endCursor);
+
+    return pageInfo;
+  })
+  // log error when there is no next page
+  .catch(console.log);
